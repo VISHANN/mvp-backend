ee57e32bc64287ddd3842b0297ab2e41863bdaa3
Ujjwal Goswami <19ucc081@lnmiit.ac.in>
2024-02-21 00:15:40 +0530

    add Review model

--- mongo/models.js
+++ mongo/models.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
   given_name: String,
@@ -12,14 +12,33 @@ const userSchema = mongoose.Schema({
     email_verified: Boolean,
   },
   shelves: {
-    0: [ String ],
-    1: [ String ],
-    2: [ String ],
-    3: [ String ],
-  }
+    0: [String],
+    1: [String],
+    2: [String],
+    3: [String],
+  },
 });
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
+
+const reviewSchema = mongoose.Schema(
+  {
+    workId: mongoose.Schema.Types.ObjectId,
+    author: mongoose.Schema.Types.ObjectId,
+    rating: String,
+    text: String,
+    moods: [String],
+    pace: String,
+  },
+  {
+    timestamps: {
+      createdAt: true,
+      updatedAt: false,
+    },
+  }
+);
+const Review = mongoose.model("Review", reviewSchema);
 
 module.exports = {
-  User: User
-}
+  User: User,
+  Review,
+};
