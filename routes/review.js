6731acf9e8dba795c668e54a98aefc59241d1270
Ujjwal Goswami <19ucc081@lnmiit.ac.in>
2024-02-20 23:36:06 +0530

    receive review at /review/:id

--- routes/review.js
+++ routes/review.js
@@ -1,59 +1,71 @@
-const express = require('express'),
-  router =  express.Router();
+const express = require("express"),
+  router = express.Router();
 
 const rating = [
   {
-    id: '0',
-    name: 'Not for me'
+    id: "0",
+    name: "dislike",
+    caption: "Not for me",
   },
   {
-    id: '1',
-    name: 'I like it'
+    id: "1",
+    name: "like",
+    caption: "I like it",
   },
   {
-    id: '2',
-    name: 'Love this!'
+    id: "2",
+    name: "love",
+    caption: "Love this!",
   },
-]
+];
 
 // NOTE: the moods ids are not sorted deliberately.
 const moods = [
-  {name: 'adventurous', id: '0'},
-  {name: 'challenging', id: '1'},
-  {name: 'dark', id: '12'},
-  {name: 'emotional', id: '3'},
-  {name: 'funny', id: '4'},
-  {name: 'hopeful', id: '5'},
-  {name: 'informative', id: '6'},
-  {name: 'inspiring', id: '7'},
-  {name: 'lighthearted', id: '8'},
-  {name: 'mysterious', id: '9'},
-  {name: 'reflective', id: '10'},
-  {name: 'relaxing', id: '11'},
-  {name: 'sad', id: '2'},
-  {name: 'tense', id: '13'},
+  { name: "adventurous", id: "0" },
+  { name: "challenging", id: "1" },
+  { name: "dark", id: "12" },
+  { name: "emotional", id: "3" },
+  { name: "funny", id: "4" },
+  { name: "hopeful", id: "5" },
+  { name: "informative", id: "6" },
+  { name: "inspiring", id: "7" },
+  { name: "lighthearted", id: "8" },
+  { name: "mysterious", id: "9" },
+  { name: "reflective", id: "10" },
+  { name: "relaxing", id: "11" },
+  { name: "sad", id: "2" },
+  { name: "tense", id: "13" },
 ];
 const pace = [
   {
-    id: '0',
-    name: 'Slow'
+    id: "0",
+    name: "Slow",
   },
   {
-    id: '1',
-    name: 'Medium'
+    id: "1",
+    name: "Medium",
   },
   {
-    id: '2',
-    name: 'Fast'
-  }
-]
+    id: "2",
+    name: "Fast",
+  },
+];
 
-router.get('/review/props', (req, res) => {
-  res.json({ 
+router.get("/review/props", (req, res) => {
+  res.json({
     rating,
     moods,
     pace,
-  })
-})
+  });
+});
+router.post("/review/:id", (req, res) => {
+  console.log(req.params.id);
+  console.log(req.body);
+
+  /*
+    req.body is review object which would be in the shape
+  */
+  res.json(req.body);
+});
 
 module.exports = router;
