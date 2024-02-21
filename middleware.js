2d24e5d6f4a1ceee37be99d56a64262ce7ce4100
Ujjwal Goswami <19ucc081@lnmiit.ac.in>
2024-02-22 00:43:53 +0530

    consolidate error responses

--- middleware.js
+++ middleware.js
@@ -2,23 +2,23 @@ function isAuthenticated(req, res, next) {
   if (!req.session.user) {
     // User is un-authenticated and should be redirected to login
     return res.status(401).json({
-      errCode: 'user_not_authenticated',
-      errText: "User is not authenticated. Please log in"
-    })
+      code: "user_not_authenticated",
+      text: "User is not authenticated. Please log in",
+    });
   }
 
   if (req.session.user && req.session.user.sub) {
     // User clicked GoogleSignIn button. Since the user in not registered
-    // with us, we have initialized a session. Here we check if session is 
+    // with us, we have initialized a session. Here we check if session is
     // of that type.
     return res.status(401).json({
-      errCode: 'user_signup_incomplete',
-      errText: "User session active as the user tried logging in but didn't complete registration by username",
-    })
+      code: "user_signup_incomplete",
+      text: "User session active as the user tried logging in but didn't complete registration by username",
+    });
   }
 
   // User is authenticated, call next in stack.
   next();
 }
 
-module.exports = { isAuthenticated }
+module.exports = { isAuthenticated };
--- routes/auth.js
+++ routes/auth.js
@@ -1,90 +1,96 @@
-const express = require('express'),
-  router =  express.Router(),
-  { User } = require('../mongo/models'),
-  { isAuthenticated } = require('../middleware'),
-  { findOrCreate, purgeUser, verify, findUser } = require('./lib');
+const express = require("express"),
+  router = express.Router(),
+  { User } = require("../mongo/models"),
+  { isAuthenticated } = require("../middleware"),
+  { findOrCreate, purgeUser, verify, findUser } = require("./lib");
 
 // -----------------------------------------------------------------
 
-router.use(express.json())
+router.use(express.json());
 
 // -----------------------------------------------------------------
 
-router.get('/api/v1/me', isAuthenticated, (req, res) => {
+router.get("/api/v1/me", isAuthenticated, (req, res) => {
   const userId = req.session.user._id;
 
-  User.findOne({ '_id': userId })
-    .then(foundUser => res.json(foundUser))
-    .catch(err => res.status(401).json(err));
-})
+  User.findOne({ _id: userId })
+    .then((foundUser) => res.json(foundUser))
+    .catch((err) => res.status(401).json(err));
+});
 
-router.post('/api/v1/validate', (req, res) => {
+router.post("/api/v1/validate", (req, res) => {
   const username = req.body.username;
-  
-  // if username is not between 3-20 char
- if ( username.length < 3 || username.length > 20 ) {
-  res.json({ isValid: false, code: 'Username Invalid' })
- }
 
-  User.findOne({ 'username': username })
-    .then(foundUser => handleFoundUser(foundUser))
-    .catch(err => res.status(401).json('DB Error'.toUpperCase))
-  
+  // if username is not between 3-20 char
+  if (username.length < 3 || username.length > 20) {
+    // try to convert to: { code: "username_invalid", text: "Username already exists. "}
+    res.json({ isValid: false, code: "Username Invalid" });
+  }
+
+  User.findOne({ username: username })
+    .then((foundUser) => handleFoundUser(foundUser))
+    .catch((err) => res.status(401).json("DB Error".toUpperCase));
+  // res.status(401).json({ code: 'db_read_error', text: 'There was internal issue while finding User' })
+
   function handleFoundUser(foundUser) {
-    if(foundUser) {
-      res.json({ isValid: false, code: 'Username Invalid' })
+    if (foundUser) {
+      // try to convert to: { code: "username_invalid", text: "Username already exists. "}
+      res.json({ isValid: false, code: "Username Invalid" });
     } else {
       // findOne returns empty doc when no doc matches.
-      res.json({ isValid: true, code: 'Username Valid' })
+      // try to convert to: { code: "username_invalid", text: "Username already exists. "}
+      res.json({ isValid: true, code: "Username Valid" });
     }
   }
-})
-router.get('/api/signin', (req, res) => {
-  const authorizationHeader = req.headers['authorization'];
-  
-  if (authorizationHeader.startsWith('Bearer ')) {
-    const jwtToken = authorizationHeader.split(' ')[1];
+});
+router.get("/api/signin", (req, res) => {
+  const authorizationHeader = req.headers["authorization"];
+
+  if (authorizationHeader.startsWith("Bearer ")) {
+    const jwtToken = authorizationHeader.split(" ")[1];
 
     verify(jwtToken)
-      .then(user => {
+      .then((user) => {
         return new Promise((resolve, reject) => {
           findUser(purgeUser(user))
-            .then(foundUser => {
+            .then((foundUser) => {
               // user is not registered as mongoose returns empty doc when no match found
 
               if (!foundUser) {
                 req.session.user = user;
 
                 throw new Error({
-                  errorCode: 'user_not_registered',
-                  errorMessage: 'User is not registered, let user to Sign Up by choosing a unique username.'
-                })
+                  code: "user_not_registered",
+                  text: "User is not registered, let user Sign Up by choosing a unique username.",
+                });
               } else {
                 resolve(foundUser);
               }
             })
-            .catch(err => reject(err))
-        })
+            .catch((err) => reject(err));
+        });
       })
-      .then(foundUser => {
-        const {_id, given_name} = foundUser;
-        req.session.user = { _id, given_name};
+      .then((foundUser) => {
+        const { _id, given_name } = foundUser;
+        req.session.user = { _id, given_name };
 
         return res.json(foundUser);
       })
-      .catch(err => {
-        console.error('/routes/auth.js: signin_100 \n');
+      .catch((err) => {
+        console.error("/routes/auth.js: signin_100 \n");
         res.status(401).json(err);
-      })
-
+      });
   } else {
-    res.status(401).json({ statusText: 'Incorrect Authorization header configuration'.toUpperCase()}); 
+    res.status(401).json({
+      code: "bad_authorization_token",
+      text: "Bearer Token sent as Authorization header is invalid.",
+    });
   }
-})
+});
 
-router.post('/api/v1/signup', (req, res) => {
+router.post("/api/v1/signup", (req, res) => {
   // check if user has a session, which was setup by /api/signin.
-  // purgedUser was stored at req.session.user 
+  // purgedUser was stored at req.session.user
   const user = req.session.user;
   const { username } = req.body;
 
@@ -97,46 +103,45 @@ router.post('/api/v1/signup', (req, res) => {
     1: [],
     2: [],
     3: [],
-  }
+  };
 
   findOrCreate(purgedUser)
-    .then(returnedUser => {
+    .then((returnedUser) => {
       // replace with post-login session
-      const {_id, given_name} = returnedUser;
-      req.session.user = { _id, given_name};
+      const { _id, given_name } = returnedUser;
+      req.session.user = { _id, given_name };
 
       return res.json(returnedUser);
     })
-    .catch(err => {
-      console.log('/routes/auth.js: Line 30 \n ' + err);
+    .catch((err) => {
+      console.log("/routes/auth.js: Line 30 \n " + err);
       res.status(404).json(err);
-    })
+    });
 });
 
-router.post('/api/v1/signout', isAuthenticated, (req, res) => {
-
+router.post("/api/v1/signout", isAuthenticated, (req, res) => {
   req.session.destroy((err) => {
     if (err) {
       res.status(401).json({
-        code: 'logout_unsuccessful',
-        text: 'We could not log you out. Please try logging out again'
+        code: "logout_unsuccessful",
+        text: "We could not log you out. Please try logging out again",
       });
     }
 
     res.json({
-      code: 'logout_successful',
-      text: 'You have successfully logged out. We will miss you.'
-    })
-  })
+      code: "logout_successful",
+      text: "You have successfully logged out. We will miss you.",
+    });
+  });
 });
 
 // router.post('/api/v1/signup', (req, res) => {
 //   const authorizationHeader = req.headers['authorization'];
 //   const username = req.body.username;
-  
+
 //   if (authorizationHeader.startsWith('Bearer ')) {
 //     const jwtToken = authorizationHeader.split(' ')[1];
-    
+
 //     verify(jwtToken)
 //       .then(user => {
 
@@ -159,7 +164,7 @@ router.post('/api/v1/signout', isAuthenticated, (req, res) => {
 //       })
 
 //   } else {
-//     res.status(401).json({ statusText: 'Incorrect Authorization header configuration'.toUpperCase()}); 
+//     res.status(401).json({ statusText: 'Incorrect Authorization header configuration'.toUpperCase()});
 //   }
 // })
 
