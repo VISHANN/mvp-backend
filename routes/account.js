0d93ba9d1b84fb0b10a4f811f819731114bf4330
Ujjwal Goswami <19ucc081@lnmiit.ac.in>
2024-04-17 04:14:29 +0530

    add update route for account/edit

--- routes/account.js
+++ routes/account.js
@@ -1,8 +1,12 @@
-const router = require("express").Router();
+const express = require("express");
+const router = express.Router();
 const { isAuthenticated } = require("../middleware");
 const { User } = require("../mongo/models");
 
-// routes
+// --------------------------------------------------------------------------------
+
+// --------------------------------------------------------------------------------
+
 router.get("/edit", isAuthenticated, (req, res) => {
   User.findOne({ _id: req.session.user._id })
     .then((user) => {
@@ -27,9 +31,60 @@ router.get("/edit", isAuthenticated, (req, res) => {
     .catch((err) => {
       res.status(500).json({
         code: "db_read_unsuccessful",
-        text: "Error occurred while trying to read User from database",
+        text: "Error occurred while trying to read user from database",
       });
     });
 });
+
+// POST route for updating account settings
+router.put("/edit", isAuthenticated, (req, res) => {
+  // req.body ==>
+  //
+  // {
+  //   profile: {
+  //     username: 'uzi',
+  //     given_name: 'Ujjwal',
+  //     family_name: 'Goswami',
+  //     picture: 'https://lh3.googleusercontent.com/a/ACg8ocL3obtbp-ilriIlZeHDDPhpxxPilPhZwzqELo5H6NLr=s96-c',
+  //     bio: '',
+  //     gender: '0'
+  //   }
+  // }
+
+  const { given_name, family_name, bio, gender } = req.body.profile;
+  User.findOneAndUpdate(
+    { _id: req.session.user._id },
+    {
+      given_name,
+      family_name,
+      profile: {
+        bio,
+        gender,
+      },
+    },
+    {
+      returnOriginal: false,
+    }
+  )
+    .then((updatedUser) => {
+      if (!updatedUser) {
+        res.status(451).json({
+          code: "user_account_deleted",
+          text: "The user account associated with this session has been deleted.",
+        });
+      }
+
+      // else user has been updated
+      res.json(updatedUser);
+    })
+    .catch((err) =>
+      res.status(500).json({
+        code: "db_write_unsuccessful",
+        text: "Error occurred while trying to update user to database",
+      })
+    );
+});
+
+// --------------------------------------------------------------------------------
 
 module.exports = router;
