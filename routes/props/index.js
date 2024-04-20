36a3e0dfb16421acd46e9946ed2f5ebf3cb50cfb
Ujjwal Goswami <19ucc081@lnmiit.ac.in>
2024-04-20 21:22:42 +0530

    serve shelves props from /props

--- routes/props/index.js
+++ routes/props/index.js
@@ -2,10 +2,16 @@ const router = require("express").Router();
 const { isAuthenticated } = require("../../middleware");
 
 // Import data jsons from ./data
+const shelvesProps = require("./data/shelves.json");
 const accountProps = require("./data/account");
 const reviewProps = require("./data/review");
 
 // ------------------------------------------------------
+
+// serve shelves props
+router.get("/shelves", (req, res) => {
+  res.json(shelvesProps);
+});
 
 // serve account props for /settings page
 router.get("/account/edit", isAuthenticated, (req, res) => {
